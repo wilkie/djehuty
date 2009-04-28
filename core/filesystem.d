fbbb0103c6a4d4f12b9b5b10e17c2588ad35ed15
wilkie <wilkie05@gmail.com>
2009-04-28 14:44:52 -0400

    Added Windows Platform Implementation Stubs for Directory Scaffold.

--- core/filesystem.d
+++ core/filesystem.d
@@ -13,11 +13,15 @@ class FileSystem
 public:
 static:
 
+	// Description: This function will return the String representing the current directory.
+	// Returns: The Directory representing the working directory.
 	Directory getCurrentDirectory()
 	{
 		return new Directory(Scaffold.DirectoryGetCWD());
 	}
 
+	// Description: This function will return the String representing the directory the executable is located in.
+	// Returns: The Directory representing the executable location.
 	Directory getApplicationDirectory()
 	{
 		return new Directory(Scaffold.DirectoryGetApp());
