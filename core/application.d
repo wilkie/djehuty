533ad6b6be69c6ec1a0b14ab8e8c1f1d0b4212c7
wilkie <wilkie05@gmail.com>
2009-05-20 16:08:03 -0400

    Added the Application class. I think it will aid controller design.

---
+++ core/application.d
@@ -0,0 +1,20 @@
+/*
+ * application.d
+ *
+ * This file contains the logic behind the main Application class.
+ *
+ * Author: Dave Wilkinson
+ * Originated: May 20th, 2009
+ *
+ */
+
+module core.application;
+
+// Description: This class represents the application instance.
+class Application {
+
+	this() {
+	}
+	
+
+}
