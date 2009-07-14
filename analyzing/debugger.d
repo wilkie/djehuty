bcc680b8adeaf3319ef101df4656b2e393495eb8
wilkie <wilkie05@gmail.com>
2009-07-14 04:01:54 -0400

    Prettied Debugger

--- analyzing/debugger.d
+++ analyzing/debugger.d
@@ -29,52 +29,43 @@ class Debugger
 static:
 public:
 
-	void raiseException(Exception e, Window w = null, Thread t = null)
-	{
-		if (_delegate !is null)
-		{
+	void raiseException(Exception e, Window w = null, Thread t = null) {
+		if (_delegate !is null) {
 			// Wouldn't it be ridiculous if the handler threw an exception?
-			try
-			{
+			try {
 				_delegate(e);
 				return;
 			}
-			catch(Exception exp)
-			{
+			catch(Exception exp) {
 				// no chance for the handler to be reentered in this case
 				e = exp;
 			}
 		}
 
 		Console.setColor(fgColor.BrightRed);
-		if (t is null)
-		{
+		
+		if (t is null) {
 			Console.putln("Unhandled Main Exception: ", e.toString());
 		}
-		else
-		{
+		else {
 			Console.putln("Unhandled Thread Exception: ", e.toString());
 		}
 
-		version(LDC)
-		{
+		version(LDC) {
 			// Tango provides a traceback for us
 			Console.putln("    from file: ", e.file);
 		}
-		else
-		{
+		else {
 		}
 
-		if (w !is null)
-		{
+		if (w !is null) {
 			// get class name
 			ClassInfo ci = w.classinfo;
 			String className = new String(ci.name);
 
 			Console.putln("    from window: ", className.array, " [", w.text.array, "]");
 		}
-		if (t !is null)
-		{
+		if (t !is null) {
 			// get class name
 			ClassInfo ci = t.classinfo;
 			String className = new String(ci.name);
@@ -85,9 +76,12 @@ public:
 		Console.setColor(fgColor.White);
 	}
 
-	void setDelegate(void delegate(Exception) newDelegate)
-	{
+	void receiver(void delegate(Exception) newDelegate) {
 		_delegate = newDelegate;
+	}
+
+	void delegate(Exception) receiver() {
+		return _delegate;
 	}
 
 	void raiseSignal(uint signal) {
