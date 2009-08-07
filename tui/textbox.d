ed1759f0335872433d8d3463ce523f35271e6d67
wilkie <wilkie05@gmail.com>
2009-08-07 14:08:11 -0400

    Fixed mp3Huffman for DMD 1.046 due to compiler bug introduced in this release. Added character appending and removing to the Tui TextBox widget.

--- tui/textbox.d
+++ tui/textbox.d
@@ -25,15 +25,48 @@ class TuiTextBox : TuiWidget {
 
 		_lines = new ArrayList!(String);
 		_lines.addItem(new String("Hello"));
-		_lines.addItem(new String("Bugger"));
+		for (int o; o < 500; o++) {
+			_lines.addItem(new String(o));
+		}
 	}
 
-	void onInit() {
+	override void onInit() {
 		draw();
+		positionCaret();
 	}
 
-	void onKeyDown(uint keyCode) {
+	override void onKeyDown(uint keyCode) {
 		switch (keyCode) {
+			case KeyBackspace:
+				if (_column == 0) {
+					_row--;
+					if (_row < 0) {
+						break;
+					}
+
+					_column = _lines[_row].length;
+					_lines[_row] = _lines[_row] ~ _lines[_row+1];
+					String oldLine;
+					_lines.remove(oldLine, _row+1);
+					draw();
+					positionCaret();
+					break;
+				}
+				else if (_column == 1) {
+					_lines[_row] = _lines[_row].subString(1);
+				}
+				else if (_column == _lines[_row].length) {
+					_lines[_row] = _lines[_row].subString(0, _lines[_row].length - 1);
+				}
+				else {
+					_lines[_row] = _lines[_row].subString(0, _column-1) ~ _lines[_row].subString(_column);
+				}
+				
+				_column--;
+
+				drawLine(_row);
+				positionCaret();
+				break;
 			case KeyArrowLeft:
 				_column--;
 				if (_column < 0) {
@@ -99,8 +132,65 @@ class TuiTextBox : TuiWidget {
 		}
 	}
 
+	override void onKeyChar(dchar chr) {
+		_lines[_row] = _lines[_row].subString(0, _column) ~ [chr] ~ _lines[_row].subString(_column);
+		drawLine(_row);
+		_column++;
+		positionCaret();
+	}
+
 	void positionCaret() {
-		Console.setPosition(this.left + (_column - _firstColumn), this.top + (_row - _firstVisible));
+		bool shouldDraw;
+
+		if (_column < _firstColumn) {
+			// scroll horizontally
+			_firstColumn = _column;
+			if (_firstColumn < 0) {
+				_firstColumn = 0;
+			}
+			shouldDraw = true;
+		}
+
+		if (this.left + (_column - _firstColumn) >= this.right) {
+			// scroll horizontally
+			_firstColumn = _column - this.width + 1;
+			shouldDraw = true;
+		}
+
+		if (_row < _firstVisible) {
+			// scroll vertically
+			_firstVisible = _row;
+			if (_firstVisible < 0) {
+				_firstVisible = 0;
+			}
+			shouldDraw = true;
+		}
+
+		if (this.top + (_row - _firstVisible) >= this.bottom) {
+			// scroll vertically
+			_firstVisible = _row - this.height + 1;
+			if (_firstVisible >= _lines.length) {
+				_firstVisible = _lines.length - 1;
+			}
+			shouldDraw = true;
+		}
+
+		if (shouldDraw) {
+			draw();
+		}
+
+		// Is the caret on the screen?
+		if ((this.left + (_column - _firstColumn) >= this.right) || (this.top + (_row - _firstVisible) >= this.bottom)) {
+			// The caret is outside of the bounds of the widget
+			Console.hideCaret();
+		}
+		else {
+			// The caret is within the bounds of the widget
+			Console.showCaret();
+
+			// Move cursor to where the edit caret is
+			Console.setPosition(this.left + (_column - _firstColumn), this.top + (_row - _firstVisible));
+		}
 	}
 
 	uint row() {
@@ -116,37 +206,28 @@ protected:
 	void draw() {
 		// Draw each line and pad any remaining spaces
 
-		uint i, k;
+		uint i;
 
 		for (i = _firstVisible; i < _lines.length && i < _firstVisible + this.height; i++) {
 			// Draw line
-			Console.setPosition(this.left, this.top + (i - _firstVisible));
-			Console.put(_lines[i]);
-
-			// Pad with spaces
-			for (k = _lines[i].length; k < this.right; k++) {
-				Console.put(" ");
-			}
+			drawLine(i);
 		}
+	}
 
-		// Is the caret on the screen?
-		if ((this.left + _column >= this.right) || (this.top + _row >= this.bottom)) {
-			// The caret is outside of the bounds of the widget
-			Console.hideCaret();
-		}
-		else {
-			// The caret is within the bounds of the widget
-			Console.showCaret();
+	void drawLine(uint lineNumber) {
+		Console.setPosition(this.left, this.top + (lineNumber - _firstVisible));
+		Console.put(_lines[lineNumber]);
 
-			// Move cursor to where the edit caret is
-			Console.setPosition(this.left + _column, this.top + _row);
+		// Pad with spaces
+		for (uint k = _lines[lineNumber].length; k < this.right; k++) {
+			Console.put(" ");
 		}
 	}
 
 	ArrayList!(String) _lines;
 
-	uint _firstVisible;
-	uint _firstColumn;
+	int _firstVisible;
+	int _firstColumn;
 
 	int _row;
 	int _column;
