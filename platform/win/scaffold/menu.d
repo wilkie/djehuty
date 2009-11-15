1f17d87b23dfdca73b498a0d1916919c0ea78846
LindseyB <hazardous.narayan@gmail.com>
2009-11-15 03:36:31 -0500

    Added code to support gui menu separators in Windows

--- platform/win/scaffold/menu.d
+++ platform/win/scaffold/menu.d
@@ -28,47 +28,60 @@ import gui.window;
 
 import resource.menu;
 
-void MenuCreate(MenuPlatformVars* menuVars)
-{
+void MenuCreate(MenuPlatformVars* menuVars) {
 	menuVars.hMenu = CreateMenu();
 }
 
-void MenuDestroy(MenuPlatformVars* menuVars)
-{
+void MenuDestroy(MenuPlatformVars* menuVars) {
 	DestroyMenu(menuVars.hMenu);
 	menuVars.hMenu = null;
 }
 
-void MenuAppend(void* identifier, MenuPlatformVars* mnuVars, MenuPlatformVars* toAppendVars, String text, bool hasSubitems)
-{
-	String s = new String(text);
-	s.appendChar('\0');
-	if (hasSubitems)
-	{
-		AppendMenuW(mnuVars.hMenu,MF_POPUP,cast(uint*)toAppendVars.hMenu,s.ptr);
+void MenuAppend(void* identifier, MenuPlatformVars* mnuVars, MenuPlatformVars* toAppendVars, String text, bool hasSubitems) {
+	String s;
+	if (text == "") {
+		if (hasSubitems) {
+			AppendMenuW(mnuVars.hMenu,MF_SEPARATOR,cast(uint*)toAppendVars.hMenu,"\0"w.ptr);
+		}
+		else {
+			AppendMenuW(mnuVars.hMenu,MF_SEPARATOR,cast(uint*)identifier,"\0"w.ptr);
+		}
 	}
-	else
-	{
-		AppendMenuW(mnuVars.hMenu,MF_STRING,cast(uint*)identifier,s.ptr);
-	}
-}
-
-void MenuUpdate(void* identifier, MenuPlatformVars* mnuVars, MenuPlatformVars* toUpdateVars, String text, uint position, bool hasSubitems)
-{
-	String s = new String(text);
-	s.appendChar('\0');
-	if (hasSubitems)
-	{
-		ModifyMenuW(mnuVars.hMenu,position,MF_BYPOSITION | MF_POPUP,cast(uint*)toUpdateVars.hMenu,s.ptr);
-	}
-	else
-	{
-		ModifyMenuW(mnuVars.hMenu,position,MF_BYPOSITION | MF_STRING,cast(uint*)identifier,s.ptr);
+	else {
+		s = new String(text);
+		s.appendChar('\0');
+		if (hasSubitems) {
+			AppendMenuW(mnuVars.hMenu,MF_POPUP,cast(uint*)toAppendVars.hMenu,s.ptr);
+		}
+		else {
+			AppendMenuW(mnuVars.hMenu,MF_STRING,cast(uint*)identifier,s.ptr);
+		}
 	}
 }
 
-void WindowSetMenu(MenuPlatformVars* mnuVars, ref Window wnd, WindowPlatformVars* windowVars)
-{
+void MenuUpdate(void* identifier, MenuPlatformVars* mnuVars, MenuPlatformVars* toUpdateVars, String text, uint position, bool hasSubitems) {
+	String s;
+	if (text.trim() == "") {
+		if (hasSubitems) {
+			ModifyMenuW(mnuVars.hMenu,position,MF_BYPOSITION | MF_SEPARATOR,cast(uint*)toUpdateVars.hMenu,"\0"w.ptr);
+		}
+		else {
+			ModifyMenuW(mnuVars.hMenu,position,MF_BYPOSITION | MF_SEPARATOR,cast(uint*)identifier,"\0"w.ptr);
+		}
+	}
+	else {
+		s = new String(text);
+		s.appendChar('\0');
+		if (hasSubitems) {
+			ModifyMenuW(mnuVars.hMenu,position,MF_BYPOSITION | MF_POPUP,cast(uint*)toUpdateVars.hMenu,s.ptr);
+		}
+		else {
+			ModifyMenuW(mnuVars.hMenu,position,MF_BYPOSITION | MF_STRING,cast(uint*)identifier,s.ptr);
+		}
+	}
+}
+
+void WindowSetMenu(MenuPlatformVars* mnuVars, ref Window wnd, WindowPlatformVars* windowVars) {
 	SetMenu(windowVars.hWnd, mnuVars.hMenu);
 
 	// resize to adapt client area
