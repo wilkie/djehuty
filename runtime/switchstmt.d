657fe63e0e0fac7c4e7266e5e5fa5d6bd37920ff
wilkie <wilkie@wilkie-desktop.(none)>
2010-03-08 19:42:34 -0500

    Described switch runtime functions.

--- runtime/switchstmt.d
+++ runtime/switchstmt.d
@@ -10,14 +10,26 @@ module runtime.switchstmt;
 
 extern(C):
 
+// Description: This runtime function will search the given sorted list of
+//   UTF8 strings and locate the string given.
+// Returns: The index into table where the given string was located and -1
+//   if it was not found.
 int _d_switch_string(char[][] table, char[] ca) {
 	return 0;
 }
 
+// Description: This runtime function will search the given sorted list of
+//   UTF16 strings and locate the string given.
+// Returns: The index into table where the given string was located and -1
+//   if it was not found.
 int _d_switch_ustring(wchar[][] table, wchar[] ca) {
 	return 0;
 }
 
+// Description: This runtime function will search the given sorted list of
+//   UTF32 strings and locate the string given.
+// Returns: The index into table where the given string was located and -1
+//   if it was not found.
 int _d_switch_dstring(dchar[][] table, dchar[] ca) {
 	return 0;
 }
