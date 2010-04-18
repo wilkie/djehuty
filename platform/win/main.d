ca69c8557960c9fee4621184caea81251310e687
LindseyB <hazardous.narayan@gmail.com>
2010-04-18 01:55:37 -0400

    Removed redundant constructor for Regex class in Core. Phobos seems to be lacking a function export, as weird as that is. So it has been statically added to windows main. Sad. FACE.

--- platform/win/main.d
+++ platform/win/main.d
@@ -25,6 +25,80 @@ import analyzing.debugger;
 
 import platform.application;
 
+// Convert real in ST0 to ulong
+
+private real adjust = cast(real)0x800_0000_0000_0000 * 0x10;
+
+private short roundTo0 = 0xFBF;
+
+extern(C) ulong __LDBLULLNG()
+{
+    version (OSX)
+    {
+	asm
+	{   naked				;
+	    push	0xFBF			; // roundTo0
+	    push	0x0000403e		;
+	    push	0x80000000		;
+	    push	0			; // adjust
+	    sub		ESP,16			;
+	    fld		real ptr 16[ESP]	; // adjust
+	    fcomp				;
+	    fstsw	AX			;
+	    fstcw	8[ESP]			;
+	    fldcw	28[ESP]			; // roundTo0
+	    sahf				;
+	    jae		L1			;
+	    fld		real ptr 16[ESP]	; // adjust
+	    fsubp	ST(1), ST		;
+	    fistp	qword ptr [ESP]		;
+	    pop		EAX			;
+	    pop		EDX			;
+	    fldcw	[ESP]			;
+	    add		ESP,24			;
+	    add		EDX,0x8000_0000		;
+	    ret					;
+	L1:					;
+	    fistp	qword ptr [ESP]		;
+	    pop		EAX			;
+	    pop		EDX			;
+	    fldcw	[ESP]			;
+	    add		ESP,24			;
+	    ret					;
+	}
+    }
+    else
+    {
+	asm
+	{   naked				;
+	    sub		ESP,16			;
+	    fld		real ptr adjust		;
+	    fcomp				;
+	    fstsw	AX			;
+	    fstcw	8[ESP]			;
+	    fldcw	roundTo0		;
+	    sahf				;
+	    jae		L1			;
+	    fld		real ptr adjust		;
+	    fsubp	ST(1), ST		;
+	    fistp	qword ptr [ESP]		;
+	    pop		EAX			;
+	    pop		EDX			;
+	    fldcw	[ESP]			;
+	    add		ESP,8			;
+	    add		EDX,0x8000_0000		;
+	    ret					;
+	L1:					;
+	    fistp	qword ptr [ESP]		;
+	    pop		EAX			;
+	    pop		EDX			;
+	    fldcw	[ESP]			;
+	    add		ESP,8			;
+	    ret					;
+	}
+    }
+}
+
 extern (C) void gc_init();
 extern (C) void gc_term();
 extern (C) void _minit();
