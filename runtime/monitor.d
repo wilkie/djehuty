b4b7cf01ea86d24bdfd31aa4f7a2e85c5a3e7680
wilkie <wilkie05@gmail.com>
2010-05-16 18:25:26 -0400

    Fixed Monitor to allow the owning Thread to not block itself.

--- runtime/monitor.d
+++ runtime/monitor.d
@@ -13,13 +13,18 @@ module runtime.monitor;
 import runtime.gc;
 
 import synch.semaphore;
+import synch.thread;
+import synch.atomic;
+
 import io.console;
 
 extern(C):
 
 struct Monitor {
 	Semaphore semaphore;
-}
+	Thread owner;
+	ulong count;
+	}
 
 void _d_monitorenter(Object h) {
 	// The monitor object is the second pointer in the object
@@ -27,16 +32,32 @@ void _d_monitorenter(Object h) {
 	if (monitor is null) {
 		monitor = new Monitor;
 		monitor.semaphore = new Semaphore(1);
+		monitor.owner = Thread.current;
+		monitor.count = 1;
 		// TODO: Should be an atomic exchange with null, and if it fails then 
 		// proceed to use that object.
 		*(cast(Monitor**)h + 1) = monitor;
+		monitor.semaphore.down();
 	}
-	monitor.semaphore.down();
+	else if (monitor.owner !is Thread.current) {
+		monitor.semaphore.down();
+	}
+	else {
+		Atomic.increment(monitor.count);
+	}
 }
 
 void _d_monitorexit(Object h) {
 	Monitor* monitor = *(cast(Monitor**)h + 1);
-	monitor.semaphore.up();
+	if (monitor.owner is Thread.current) {
+		Atomic.decrement(monitor.count);
+		if (monitor.count == 0) {
+			monitor.semaphore.up();
+		}
+	}
+	else {
+		monitor.semaphore.up();
+	}
 }
 
 void _d_criticalenter(void* dcs) {
