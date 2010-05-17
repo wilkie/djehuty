0395130157fd4dc94c86c56765c8a8e965f07323
wilkie <wilkie05@gmail.com>
2010-05-17 01:06:48 -0400

    Templated absolute value function.

--- math/common.d
+++ math/common.d
@@ -464,17 +464,26 @@ double tan(double x) {
 	return x;
 }
 
-double abs(double x) {
-	ulong intRepresentation = *cast(ulong*)&x;
-	intRepresentation &= 0x7fff_ffff_ffff_ffff;
-	return *cast(double*)&intRepresentation;
-}
-
-long abs(long x) {
-	if (x < 0) {
-		return -x;
+template abs(T) {
+	T abs(T x) {
+		static if (IsMathObject!(T)) {
+			return x.opAbs();
+		}
+		else static if (is(T : long)) {
+			if (x < 0) {
+				return -x;
+			}
+			return x;
+		}
+		else static if (is(T : double)) {
+			ulong intRepresentation = *cast(ulong*)&x;
+			intRepresentation &= 0x7fff_ffff_ffff_ffff;
+			return *cast(double*)&intRepresentation;
+		}
+		else {
+			static assert(false, "Math function abs not defined for type " ~ T.stringof);
+		}
 	}
-	return x;
 }
 
 double pow(double x, double y) {
