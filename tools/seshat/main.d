99a068ff724435ce4428edc41c33c2066a899fb3
wilkie <wilkie05@gmail.com>
2010-05-23 01:19:45 -0400

    Updated Seshat with some comments and colored output for fun.

--- tools/seshat/main.d
+++ tools/seshat/main.d
@@ -14,8 +14,14 @@ class Seshat : Application {
 		_options = new Opts();
 
 		if (_options.path is null) {
+			Console.forecolor = Color.Red;
+			Console.putln("USAGE: seshat --build file.d");
 			return;
 		}
+
+		Console.forecolor = Color.Green;
+		Console.putln(" -- Dependency Crawl --");
+		Console.putln(" : " ~ _options.path);
 
 		// 0. Get dependencies
 		_list = new DependencyList(_options.path);
@@ -23,8 +29,39 @@ class Seshat : Application {
 		// 1. Build Dependencies
 		auto linkingList = new List!(string);
 
-		foreach(mod; _list) {
+		auto builtList = new List!(string);
+
+		Console.forecolor = Color.Blue;
+		Console.putln();
+		Console.putln(" -- Building --");
+		double done = 0.0;
+		foreach(ref size_t idx, string mod; _list) {
+			done = cast(double)idx / cast(double)(_list.length-1);
+			done *= 100;
+
+			int percent = cast(int)done;
+
+			string pc = "{d}%".format(percent);
+			if (pc.length < 3) {
+				pc = "  " ~ pc;
+			}
+			else if (pc.length < 4) {
+				pc = " " ~ pc;
+			}
+
+			Console.putln(" : " ~ pc ~ " " ~ mod.replace('/', '.'));
+
 			// Compile each module in the list if necessary
+			string object = mod[0..$-2] ~ ".o";
+
+			// Check the file time of the object against the source.
+
+			// if (File.time(mod) > File.time(object)) {
+			//   builtList.add(mod);
+			//   compile
+			// }
+
+			// Decide if this implies a library to link
 			if (mod.length > "binding/".length) {
 				if (mod[0..8] == "binding/") {
 					string bindpath = mod[8..$-2];
@@ -56,14 +93,21 @@ class Seshat : Application {
 
 		// 2. Verification (Link modules that have tests with front end)
 
+//		Console.forecolor = Color.Cyan;
+//		putln(" : Verifying " ~ moduleExecutable);
+
 		// 2a. Build Sobek component and run
 
 		// 3. Link
 		string moduleObject;
 		string moduleExecutable;
+		moduleExecutable = _options.path[0..$-2];
+		Console.forecolor = Color.Magenta;
+		Console.putln();
+		Console.putln(" -- Linking --");
+		putln(" : Linking " ~ moduleExecutable);
 		version(LDC) {
 			moduleObject = _options.path[0..$-2] ~ ".o";
-			moduleExecutable = _options.path[0..$-2];
 			string ldcCommand = "ldc -of" ~ moduleExecutable ~ " " ~ moduleObject;
 			foreach(mod; _list) {
 				ldcCommand ~= " " ~ mod[0..$-2] ~ ".o";
@@ -72,12 +116,8 @@ class Seshat : Application {
 				ldcCommand ~= " -L-l" ~ link;
 			}
 			ldcCommand ~= " -L-lpthread -L-lrt";
-			putln(ldcCommand);
+			//putln(ldcCommand);
 			System.execute(ldcCommand);
-		}
-
-		foreach(link; linkingList) {
-			putln(link);
 		}
 	}
 
