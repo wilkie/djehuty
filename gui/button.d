9830fcbd989fdb078882721bf0dc84e3a2e05d64
wilkie <wilkie05@gmail.com>
2010-08-15 03:26:35 -0400

    Beveled button.

--- gui/button.d
+++ gui/button.d
@@ -26,13 +26,24 @@ public:
 		Color clr = Color.Blue;
 		clr.alpha = 0.7;
 		Brush brush = new Brush(clr);
+
 		clr = Color.Black;
 		clr.alpha = 0.7;
 		Pen pen = new Pen(clr);
 
 		canvas.brush = brush;
 		canvas.pen = pen;
-		canvas.drawRectangle(0, 0, this.width, this.height);
-		canvas.drawEllipse(0, 0, this.width, this.height);
+
+		canvas.fillRectangle(0, 0, this.width, this.height);
+
+		canvas.fillRectangle(0, 0, this.width, 2);
+		canvas.fillRectangle(0, 2, 2, this.height-2);
+
+		canvas.brush = new Brush(Color.fromRGBA(0.0, 0.0, 0.5, 0.7));
+
+		canvas.fillRectangle(0, this.height-2, this.width, 2);
+		canvas.fillRectangle(this.width-2, 0, 2, this.height-2);
+
+		canvas.strokeRectangle(0, 0, this.width, this.height);
 	}
 }
