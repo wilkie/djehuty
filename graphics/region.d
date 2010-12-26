bbb995a29bd9d3721e91ab243707e66e3581c3db
wilkie <wilkie05@gmail.com>
2010-12-25 20:34:17 -0500

    Adding other stuff for triangulation routine.

--- graphics/region.d
+++ graphics/region.d
@@ -8,6 +8,7 @@ import math.random;
 import math.sign;
 import math.log;
 import math.round;
+import math.power;
 
 class Region {
 private:
@@ -102,7 +103,7 @@ public:
 	int[] ordering;
 
 	private void _generateRandomOrdering() {
-		int n = segments.length-1;
+		int n = segments.length - 1;
 
 		ordering = new int[n+1];
 
@@ -1086,6 +1087,577 @@ public:
 	}
 
 	void _findNewRoots(int segment) {
+		Segment *s = &segments[segment];
+
+		if (s.isInserted) {
+			return;
+		}
+
+		s.root0 = _locateEndpoint(s.v0, s.v1, s.root0);
+		s.root0 = trapezoids[s.root0].sink;
+
+		s.root1 = _locateEndpoint(s.v1, s.v0, s.root1);
+		s.root1 = trapezoids[s.root1].sink;  
+	}
+
+	static const int SP_SIMPLE_LRUP = 1;	/* for splitting trapezoids */
+	static const int SP_SIMPLE_LRDN = 2;
+	static const int SP_2UP_2DN     = 3;
+	static const int SP_2UP_LEFT    = 4;
+	static const int SP_2UP_RIGHT   = 5;
+	static const int SP_2DN_LEFT    = 6;
+	static const int SP_2DN_RIGHT   = 7;
+	static const int SP_NOSPLIT     = -1;	
+
+	static const int TR_FROM_UP = 1;		/* for traverse-direction */
+	static const int TR_FROM_DN = 2;
+
+	/* Main routine to get monotone polygons from the trapezoidation of 
+	 * the polygon.
+	 */
+
+	MonotoneChain mchain[]; /* Table to hold all the monotone */
+	/* polygons . Each monotone polygon */
+	/* is a circularly linked list */
+
+	VertexChain vert[]; /* chain init. information. This */
+	/* is used to decide which */
+	/* monotone polygon to split if */
+	/* there are several other */
+	/* polygons touching at the same */
+	/* vertex  */
+
+	int mon[];	/* contains position of any vertex in */
+	/* the monotone chain for the polygon */
+
+	bool visited[];
+
+	int chain_idx, op_idx, mon_idx;
+
+	private bool _insidePolygon(ref Trapezoid t) {
+		int rseg = t.rseg;
+
+		if (t.state == ST_INVALID) {
+			return false;
+		}
+
+		if (t.lseg <= 0 || t.rseg <= 0) {
+			return false;
+		}
+
+		if ((t.u0 <= 0 && t.u1 <= 0)
+				|| (t.d0 <= 0 && t.d1 <= 0)) {
+			// Triangle
+			return _greaterThan(segments[rseg].v1, segments[rseg].v0);
+		}
+
+		return false;
+	}
+
+	private int _monotonateTrapezoids() {
+		int n = segments.length - 1;
+
+		int i;
+		int tr_start;
+
+		mon = new int[segments.length];
+		mchain = new MonotoneChain[trapezoids.length];
+		vert = new VertexChain[segments.length];
+		visited = new bool[trapezoids.length];
+
+		// First, locate a trapezoid which lies inside the polygon
+		// and which is triangular
+
+		for (i = 0; i < trapezoids.length; i++) {
+			if (_insidePolygon(trapezoids[i])) {
+				break;
+			}
+		}
+
+		tr_start = i;
+
+		// Initialize the mon data-structure and start spanning all the
+		// trapezoids within the polygon
+
+		for (i = 1; i <= n; i++) {
+			mchain[i].prev = segments[i].prev;
+			mchain[i].next = segments[i].next;
+			mchain[i].vnum = i;
+
+			vert[i].pt = segments[i].v0;
+			// Next vertex
+			vert[i].vnext[0] = segments[i].next;
+			// Location of next vertex
+			vert[i].vpos[0] = i;
+			vert[i].nextfree = 1;
+		}
+
+		chain_idx = n;
+		mon_idx = 0;
+
+		// Position of any vertex in the first chain
+		mon[0] = 1;
+
+		// Traverse the polygon
+
+		if (trapezoids[tr_start].u0 > 0) {
+			_traversePolygon(0, tr_start, trapezoids[tr_start].u0, TR_FROM_UP);
+		}
+		else if (trapezoids[tr_start].d0 > 0) {
+			_traversePolygon(0, tr_start, trapezoids[tr_start].d0, TR_FROM_DN);
+		}
+
+		return newmon();
+	}
+
+	private int newmon() {
+		mon_idx++;
+		return mon_idx;
+	}
+
+	private int _newChainElement() {
+		chain_idx++;
+		return chain_idx;
+	}
+
+	private double _getAngle(ref Coord vp0, ref Coord vpnext, ref Coord vp1) {
+		Coord v0, v1;
+		v0.x = vpnext.x - vp0.x;
+		v0.y = vpnext.y - vp0.y;
+
+		v1.x = vp1.x - vp0.x;
+		v1.y = vp1.y - vp0.y;
+
+		// Cross product
+		auto cross_v0_v1 = v0.x * v1.y - v1.x * v0.y;
+
+		auto length_v1 = v1.x * v1.x + v1.y * v1.y;
+		length_v1 = Power.sqrt(length_v1);
+
+		auto length_v0 = v0.x * v0.x + v0.y * v0.y;
+		length_v0 = Power.sqrt(length_v0);
+
+		auto dot_v0_v1 = v0 * v1;
+
+		if (cross_v0_v1 >= 0) {
+			// sine is positive
+			return dot_v0_v1 / length_v0 / length_v1;
+		}
+		else {
+			return (-1.0 * dot_v0_v1 / length_v0 / length_v1) - 2;
+		}
+	}
+
+	/* (v0, v1) is the new diagonal to be added to the polygon. Find which */
+	/* chain to use and return the positions of v0 and v1 in p and q */ 
+	private int _getVertexPositions(int v0, int v1, ref int ip, ref int iq) {
+		VertexChain* vp0;
+		VertexChain* vp1;
+
+		int i;
+
+		double angle, temp;
+
+		int tp, tq;
+
+		vp0 = &vert[v0];
+		vp1 = &vert[v1];
+
+		// p is identified as follows:
+
+		// Scan from (v0, v1) rightwards until you hit the first segment
+		//   starting from v0.
+
+		// That chain is the chain of interest
+
+		angle = -4.0;
+
+		for (i = 0; i < 4; i++) {
+			if (vp0.vnext[i] <= 0) {
+				continue;
+			}
+
+			temp = _getAngle(vp0.pt, vert[vp0.vnext[i]].pt, vp1.pt);
+
+			if (temp > angle) {
+				angle = temp;
+				tp = i;
+			}
+		}
+
+		ip = tp;
+
+		// Do the same for q
+
+		angle = -4.0;
+		for (i = 0; i < 4; i++) {
+			if (vp1.vnext[i] <= 0) {
+				continue;
+			}
+
+			temp = _getAngle(vp1.pt, vert[vp1.vnext[i]].pt, vp0.pt);
+			if (temp > angle) {
+				angle = temp;
+				tq = i;
+			}
+		}
+
+		iq = tq;
+	}
+
+	/* v0 and v1 are specified in anti-clockwise order with respect to 
+	 * the current monotone polygon mcur. Split the current polygon into 
+	 * two polygons using the diagonal (v0, v1) 
+	 */
+	private int _makeNewMonotonePolygon(int mcur, int v0, int v1) {
+		int p, q, ip, iq;
+		int mnew = newmon();
+
+		int i, j, nf0, nf1;
+
+		VertexChain* vp0;
+		VertexChain* vp1;
+
+		vp0 = &vert[v0];
+		vp1 = &vert[v1];
+
+		_getVertexPositions(v0, v1, ip, iq);
+
+		p = vp0.vpos[ip];
+		q = vp1.vpos[iq];
+
+		// At this stage, we have got the positions of v0 and v1 in the
+		// desired chain.
+
+		// Now, modify the linked lists
+
+		// For the new list:
+		i = _newChainElement();
+		j = _newChainElement();
+
+		mchain[i].vnum = v0;
+		mchain[j].vnum = v1;
+
+		mchain[i].next = mchain[p].next;
+		mchain[mchain[p].next].prev = i;
+		mchain[i].prev = j;
+		mchain[i].next = i;
+		mchain[j].prev = mchain[q].prev;
+		mchain[mchain[q].prev].next = j;
+
+		mchain[p].next = q;
+		mchain[q].prev = p;
+
+		nf0 = vp0.nextfree;
+		nf1 = vp1.nextfree;
+
+		vp0.vnext[ip] = v1;
+
+		vp0.vpos[nf0] = i;
+		vp0.vnext[nf0] = mchain[mchain[i].next].vnum;
+		vp1.vpos[nf1] = j;
+		vp1.vnext[nf1] = v0;
+
+		vp0.nextfree++;
+		vp1.nextfree++;
+
+		mon[mcur] = p;
+		mon[mnew] = i;
+
+		return mnew;
+	}
+
+	private void _traversePolygon(int mcur, int trnum, int from, int dir) {
+		Trapezoid* t = &trapezoids[trnum];
+
+		int howsplit, mnew;
+		int v0, v1, v0next, v1next;
+
+		int retval, tmp;
+		bool do_switch = false;
+
+		if (trnum <= 0 || visited[trnum]) {
+			return;
+		}
+
+		visited[trnum] = true;
+	
+		// We have much more information available here
+		// rseg: goes upwards
+		// lseg: goes downwards
+
+		// Initially, assume that dir = TR_FROM_DN (from the left)
+		// Switch v0 and v1 if necessary afterwards
+
+		// Special cases for triangles with cusps at the opposite ends
+		// Take care of this first
+		if (t.u0 <= 0 && t.u1 <= 0) {
+			if (t.d0 > 0 && t.d1 > 0) {
+				// Downward opening triangle
+				v0 = trapezoids[t.d1].lseg;
+				v1 = t.lseg;
+
+				if (from == t.d1) {
+					do_switch = true;
+
+					mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+					_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+					_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+				}
+				else {
+					mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+					_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+					_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+				}
+			}
+			else {
+				// Just traverse all neighbors
+				retval = SP_NOSPLIT;
+
+				_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+				_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+				_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+				_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+			}
+		}
+		else if (t.d0 <= 0 && t.d1 <= 0) {
+			if (t.u0 > 0 && t.u1 > 0) {
+				// Upward opening triangle
+
+				v0 = t.rseg;
+				v1 = trapezoids[t.u0].rseg;
+
+				if (from == t.u1) {
+					do_switch = true;
+
+					mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+					_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+					_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+				}
+				else {
+					mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+
+					_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+					_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+				}
+			}
+			else {
+				// Just traverse all neighbors
+
+				retval = SP_NOSPLIT;
+
+				_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+				_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+				_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+				_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+			}
+		}
+		else if (t.u0 > 0 && t.u1 > 0) {
+			if (t.d0 > 0 && t.d1 > 0) {
+				// Downward + upward cusps
+				
+				v0 = trapezoids[t.d1].lseg;
+				v1 = trapezoids[t.u0].rseg;
+
+				retval = SP_2UP_2DN;
+
+				if ((dir == TR_FROM_DN && t.d1 == from) 
+						|| (dir == TR_FROM_UP && t.u1 == from)) {
+					do_switch = true;
+
+					mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+					_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+					_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+					_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+					_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+				}
+				else {
+					mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+					
+					_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+					_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+					_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+					_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+				}
+			}
+			else {
+				// Only Downward cusp
+
+				if (_equalTo(t.lo, segments[t.lseg].v1)) {
+					v0 = trapezoids[t.u0].rseg;
+					v1 = segments[t.lseg].next;
+
+					retval = SP_2UP_LEFT;
+					if (dir == TR_FROM_UP && t.u0 == from) {
+						do_switch = true;
+
+						mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+						_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+					}
+					else {
+						mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+
+						_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+					}
+				}
+				else {
+					v0 = t.rseg;
+					v1 = trapezoids[t.u0].rseg;
+
+					retval = SP_2UP_RIGHT;
+
+					if (dir == TR_FROM_UP && t.u1 == from) {
+						do_switch = true;
+
+						mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+						_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+					}
+					else {
+						mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+
+						_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+					}
+				}
+			}
+		}
+		else if (t.u0 > 0 || t.u1 > 0) {
+			// No Downward Cusp
+			if (t.d0 > 0 && t.d1 > 0) {
+				// Only upward cusp
+				if (_equalTo(t.hi, segments[t.lseg].v0)) {
+					v0 = trapezoids[t.d1].lseg;
+					v1 = t.lseg;
+
+					retval = SP_2DN_LEFT;
+
+					if (!(dir == TR_FROM_DN && t.d0 == from)) {
+						do_switch = true;
+
+						mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+						_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+					}
+					else {
+						mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+
+						_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+					}
+				}
+				else {
+					v0 = trapezoids[t.d1].lseg;
+					v1 = segments[t.rseg].next;
+
+					retval = SP_2DN_RIGHT;
+
+					if (dir == TR_FROM_DN && t.d1 == from) {
+						do_switch = true;
+						mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+						_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+					}
+					else {
+						mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+
+						_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+					}
+				}
+			}
+			else {
+				// No cusp
+
+				if (_equalTo(t.hi, segments[t.lseg].v0) 
+						&& _equalTo(t.lo, segments[t.rseg].v0)) {
+					v0 = t.rseg;
+					v1 = t.lseg;
+
+					retval = SP_SIMPLE_LRDN;
+
+					if (dir == TR_FROM_UP) {
+						do_switch = true;
+
+						mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+						_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+					}
+					else {
+						mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+
+						_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+					}
+				}
+				else if (_equalTo(t.hi, segments[t.rseg].v1)
+						&& _equalTo(t.lo, segments[t.lseg].v1)) {
+					v0 = segments[t.rseg].next;
+					v1 = segments[t.lseg].next;
+
+					retval = SP_SIMPLE_LRUP;
+
+					if (dir == TR_FROM_UP) {
+						do_switch = true;
+
+						mnew = _makeNewMonotonePolygon(mcur, v1, v0);
+
+						_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.d0, trnum, TR_FROM_UP);
+					}
+					else {
+						mnew = _makeNewMonotonePolygon(mcur, v0, v1);
+
+						_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+						_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+						_traversePolygon(mnew, t.u0, trnum, TR_FROM_DN);
+						_traversePolygon(mnew, t.u1, trnum, TR_FROM_DN);
+					}
+				}
+				else {
+					// No split possible
+					retval = SP_NOSPLIT;
+
+					_traversePolygon(mcur, t.u0, trnum, TR_FROM_DN);
+					_traversePolygon(mcur, t.d0, trnum, TR_FROM_UP);
+					_traversePolygon(mcur, t.u1, trnum, TR_FROM_DN);
+					_traversePolygon(mcur, t.d1, trnum, TR_FROM_UP);
+				}
+			}
+		}
+
+//		return retval;
 	}
 
 	void _triangulatePolygon(int[] pointsPerContour, Coord[] vertices) {
@@ -1134,6 +1706,7 @@ public:
 		_generateRandomOrdering();
 
 		_constructTrapezoids();
+		int nmonpoly = _monotonateTrapezoids();
 	}
 
 	int q_idx;
